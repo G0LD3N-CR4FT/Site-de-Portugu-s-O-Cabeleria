a38c3bd99ce9157d3225940297e60ab4b536524d
G0LD3N-CR4FT <eduardomaster.tellis@gmail.com>
2022-08-18 21:06:41 -0300

    Atualização 18/08/2022 21:08

--- js/animation.js
+++ js/animation.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $('#button').click(function(){
         $('div').removeClass('bloqueio');
         $('#button').addClass('bloqueio');
-        var duration = 60 * (1/3); // Converter para segundos
+        var duration = 60 * (1/3+1/3); // Converter para segundos
         display = document.querySelector('#timer'); // selecionando o timer
     startTimer(duration, display); // iniciando o timer
     })
@@ -25,8 +25,6 @@ function startTimer(duration, display) {
         if(minutes == 0 && seconds == 0){
             $('#timer').addClass('bloqueio');
             $('#travar').addClass('bloqueio');
-            document.getElementById('resultadoerros').value = errado;
-        document.getElementById('resultadoacertos').value = certo;
         }
     }, 1000);
 }
@@ -41,7 +39,7 @@ $(document).ready(function(event){
         $('#1alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -51,7 +49,7 @@ $(document).ready(function(event){
         $('#1alternativa-3').addClass('bloqueio');
         soma = soma+1;
         certo = certo+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -61,7 +59,7 @@ $(document).ready(function(event){
         $('#1alternativa-2').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -71,7 +69,7 @@ $(document).ready(function(event){
         $('#2alternativa-3').addClass('bloqueio');
         soma = soma+1;
         certo = certo+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -81,7 +79,7 @@ $(document).ready(function(event){
         $('#2alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -91,7 +89,7 @@ $(document).ready(function(event){
         $('#2alternativa-2').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -101,7 +99,7 @@ $(document).ready(function(event){
         $('#3alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -111,7 +109,7 @@ $(document).ready(function(event){
         $('#3alternativa-3').addClass('bloqueio');
         soma = soma+1;
         certo = certo+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -121,7 +119,7 @@ $(document).ready(function(event){
         $('#3alternativa-2').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -131,7 +129,7 @@ $(document).ready(function(event){
         $('#4alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -141,7 +139,7 @@ $(document).ready(function(event){
         $('#4alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -151,7 +149,7 @@ $(document).ready(function(event){
         $('#4alternativa-2').addClass('bloqueio');
         soma = soma+1;
         certo = certo+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -161,7 +159,7 @@ $(document).ready(function(event){
         $('#5alternativa-3').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -171,7 +169,7 @@ $(document).ready(function(event){
         $('#5alternativa-3').addClass('bloqueio');
         soma = soma+1;
         certo = certo+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
@@ -181,17 +179,198 @@ $(document).ready(function(event){
         $('#5alternativa-2').addClass('bloqueio');
         soma = soma+1;
         errado = errado+1;
-        if(soma == 5){
+        if(soma == 10){
             $('#acabar').removeClass('bloqueio');
         }
     });
 
-    var mensagem1 = 'Erros:';
+    $('#6alternativa-1').click(function(event){
+        $('#6alternativa-1').addClass('errada');
+        $('#6alternativa-2').addClass('bloqueio');
+        $('#6alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#6alternativa-2').click(function(event){
+        $('#6alternativa-2').addClass('errada');
+        $('#6alternativa-1').addClass('bloqueio');
+        $('#6alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#6alternativa-3').click(function(event){
+        $('#6alternativa-3').addClass('certa');
+        $('#6alternativa-1').addClass('bloqueio');
+        $('#6alternativa-2').addClass('bloqueio');
+        soma = soma+1;
+        certo = certo+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    
+    $('#7alternativa-1').click(function(event){
+        $('#7alternativa-1').addClass('errada');
+        $('#7alternativa-2').addClass('bloqueio');
+        $('#7alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#7alternativa-2').click(function(event){
+        $('#7alternativa-2').addClass('errada');
+        $('#7alternativa-1').addClass('bloqueio');
+        $('#7alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#7alternativa-3').click(function(event){
+        $('#7alternativa-3').addClass('certa');
+        $('#7alternativa-1').addClass('bloqueio');
+        $('#7alternativa-2').addClass('bloqueio');
+        soma = soma+1;
+        certo = certo+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+
+    
+    $('#8alternativa-1').click(function(event){
+        $('#8alternativa-1').addClass('errada');
+        $('#8alternativa-2').addClass('bloqueio');
+        $('#8alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#8alternativa-2').click(function(event){
+        $('#8alternativa-2').addClass('certa');
+        $('#8alternativa-1').addClass('bloqueio');
+        $('#8alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        certo = certo+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#8alternativa-3').click(function(event){
+        $('#8alternativa-3').addClass('errada');
+        $('#8alternativa-1').addClass('bloqueio');
+        $('#8alternativa-2').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+
+    
+    $('#9alternativa-1').click(function(event){
+        $('#9alternativa-1').addClass('certa');
+        $('#9alternativa-2').addClass('bloqueio');
+        $('#9alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        certo = certo+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#9alternativa-2').click(function(event){
+        $('#9alternativa-2').addClass('errada');
+        $('#9alternativa-1').addClass('bloqueio');
+        $('#9alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#9alternativa-3').click(function(event){
+        $('#9alternativa-3').addClass('errada');
+        $('#9alternativa-1').addClass('bloqueio');
+        $('#9alternativa-2').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+
+    
+    $('#10alternativa-1').click(function(event){
+        $('#10alternativa-1').addClass('certa');
+        $('#10alternativa-2').addClass('bloqueio');
+        $('#10alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        certo = certo+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#10alternativa-2').click(function(event){
+        $('#10alternativa-2').addClass('errada');
+        $('#10alternativa-1').addClass('bloqueio');
+        $('#10alternativa-3').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+    $('#10alternativa-3').click(function(event){
+        $('#10alternativa-3').addClass('errada');
+        $('#10alternativa-1').addClass('bloqueio');
+        $('#10alternativa-2').addClass('bloqueio');
+        soma = soma+1;
+        errado = errado+1;
+        if(soma == 10){
+            $('#acabar').removeClass('bloqueio');
+        }
+    });
+
+    var mensagem3 = 'Erros:';
     var mensagem2 = 'Acertos:';
 
     $('#acabar').click(function(event){
+        if(certo > errado && errado == 0){
+            var mensagem1 ='Caraca Machado de Assis você acertou tudo';
+            }
+            if(certo == 9 || certo == 8){
+                var mensagem1 ='Quase gabaritou, mas a matemática não perdoa';
+                }
+            if(certo < 8 && certo >=6 ){
+                var mensagem1 ='Média eu diria';
+                }
+            if(certo == 5){
+                var mensagem1 ='Tudo em perfeito equilíbrio como diria Thanos';
+                }
+            if(certo < 5 && certo >= 3){
+                var mensagem1 ='Podia ser melhor em, vai estudar';
+                }
+            if(certo < 3){
+                var mensagem1 ='Tava dormindo na apresentação né?';
+                }
+            if(errado == 10){
+                var mensagem1 ='Como você teve a capacidade de errar tudo, surpreendente';
+                }
         $('.fechar').addClass('bloqueio');
-        $('#erros').append(mensagem1, errado);
+        $('#total').append(mensagem1);
+        $('#erros').append(mensagem3, errado);
         $('#acertos').append(mensagem2, certo);
     })
 })
+
